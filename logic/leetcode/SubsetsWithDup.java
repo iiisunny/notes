37e1ceb79c94fa6965bada0461bb2c394ff673d3
WangJinqi <4487071@qq.com>
2021-04-01 09:01:30 +0800

    fix(subsetsWithDup):减少dfs变量传递

--- logic/leetcode/SubsetsWithDup.java
+++ logic/leetcode/SubsetsWithDup.java
@@ -25,23 +25,23 @@ public class SubsetsWithDup {
         Arrays.sort(nums);
         int n = nums.length;
         // 使用 visited 数组来记录哪一个元素在当前路径中被使用了
-        boolean[] visited = new boolean[n];
+        boolean[] visited = new boolean[nums.length];
         // 开始回溯
-        backtrace(nums, 0, visited, n);
+        backtrace(nums, 0, visited);
         return ans;
     }
 
-    private void backtrace(int[] nums, int start, boolean[] visited, int n) {
+    private void backtrace(int[] nums, int start, boolean[] visited) {
         // 首先加入当前路径
         ans.add(new ArrayList<>(path));
         // 从 start 开始遍历每一个元素，尝试加入路径中
-        for (int i = start; i < n; ++i) {
+        for (int i = start; i < nums.length; ++i) {
             // 如果当前元素和前一个元素相同，而且前一个元素没有被访问，说明前一个相同的元素在当前层已经被用过了
             if (i > 0 && nums[i - 1] == nums[i] && !visited[i - 1]) continue;
             // 记录下来，用过了当前的元素
             visited[i] = true;
             path.add(nums[i]); // 放到路径中
-            backtrace(nums, i + 1, visited, n); // 向下一个递归
+            backtrace(nums, i + 1, visited); // 向下一个递归
             visited[i] = false; // 回溯
             path.remove(path.size() - 1);
         }

3f0cd8366b04396938140fb26fa21333e85a0d39
WangJinqi <4487071@qq.com>
2021-04-20 21:00:53 +0800

    滑动窗口的最大值、n个骰子的点数

---
+++ logic/jianzhi/Test67.java
@@ -0,0 +1,66 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 滑动窗口的最大值
+ * 给定一个数组 nums 和滑动窗口的大小 k，请找出所有滑动窗口里的最大值。
+ *
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7]
+ * 解释:
+ *
+ *   滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+ *
+ */
+public class Test67 {
+
+    /**
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(k)  双端队列 dequedeque 中最多同时存储 kk 个元素（即窗口大小）
+     * @param nums
+     * @param k
+     * @return
+     */
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums.length == 0 || k == 0) {
+            return new int[0];
+        }
+        Deque<Integer> deque = new LinkedList<>();
+
+        // 滑动窗口的个数
+        int[] res = new int[nums.length - k + 1];
+
+        // 未形成窗口
+        for(int i = 0; i < k; i++) {
+            while(!deque.isEmpty() && deque.peekLast() < nums[i]){
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+        }
+        res[0] = deque.peekFirst();
+        // 形成窗口后
+        for(int i = k; i < nums.length; i++) {
+            if(deque.peekFirst() == nums[i - k]){
+                deque.removeFirst();
+            }
+            while(!deque.isEmpty() && deque.peekLast() < nums[i]){
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            res[i - k + 1] = deque.peekFirst();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int[] arr = maxSlidingWindow(nums,3);
+    }
+}

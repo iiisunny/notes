872d55553933979c55b1e47e55449c5b5dedb365
WangJinqi <4487071@qq.com>
2021-04-22 20:34:31 +0800

    构建乘积数组

---
+++ logic/jianzhi/Test71.java
@@ -0,0 +1,31 @@
+/**
+ * 构建乘积数组
+ * 给定一个数组 A[0,1,…,n-1]，请构建一个数组 B[0,1,…,n-1]，
+ * 其中B[i] 的值是数组 A 中除了下标 i 以外的元素的积, 即B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法。
+ *
+ * 输入: [1,2,3,4,5]
+ * 输出: [120,60,40,30,24]
+ *
+ */
+public class Test71 {
+    public int[] constructArr(int[] a) {
+        if (a == null || a.length == 0) return a;
+        int len = a.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+        left[0] = right[len - 1] = 1;
+
+        for (int i = 1; i < len; i++) {
+            left[i] = left[i - 1] * a[i - 1];
+        }
+        for (int i = len - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * a[i + 1];
+        }
+
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i++) {
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
+    }
+}
